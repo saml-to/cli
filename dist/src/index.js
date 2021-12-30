9c73b7bcf38d3310be06fc6cef4d37640355934f
Christian Nuss <christian.nuss@gmail.com>
2021-12-30 05:33:55 +0200

    remove dist

--- dist/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.comingSoon = void 0;
-const comingSoon = () => {
-    return 'The SAML.to CLI is coming soon!';
-};
-exports.comingSoon = comingSoon;
-//# sourceMappingURL=index.js.map
