8bb5c40204992f9dc01b5049bdb57b67c53cad7f
Christian Nuss <christian.nuss@gmail.com>
2022-01-16 20:28:44 -0800

    handle errors

--- src/helpers/browserHelper.ts
+++ src/helpers/browserHelper.ts
@@ -2,18 +2,22 @@ import open from 'open';
 import { ui } from '../command';
 
 export const openBrowser = (url: string): Promise<void> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     ui.updateBottomBar('');
     open(url, {
       wait: true,
-    }).then((proc) => {
-      if (proc.exitCode !== 0) {
-        ui.updateBottomBar('');
-        console.log(url);
-        resolve();
-      } else {
-        resolve();
-      }
-    });
+    })
+      .then((proc) => {
+        if (proc.exitCode !== 0) {
+          ui.updateBottomBar('');
+          console.log(url);
+          resolve();
+        } else {
+          resolve();
+        }
+      })
+      .catch((e) => {
+        reject(e);
+      });
   });
 };
