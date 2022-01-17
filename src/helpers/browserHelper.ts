903014e5a42fead0c14adaf3fa0eb97452f9e237
Christian Nuss <christian.nuss@gmail.com>
2022-01-16 19:17:08 -0800

    always wait

--- src/helpers/browserHelper.ts
+++ src/helpers/browserHelper.ts
@@ -4,34 +4,17 @@ import { ui } from '../command';
 export const openBrowser = (url: string): Promise<void> => {
   return new Promise((resolve) => {
     ui.updateBottomBar('');
-    const wait = process.platform !== 'win32' && process.platform !== 'darwin';
     open(url, {
-      wait,
+      wait: true,
     }).then((proc) => {
       console.log('Platform:', process.platform);
-      if (process.platform === 'win32') {
-        proc.addListener('exit', () => {
-          resolve();
-        });
-      } else if (wait && proc.exitCode !== 0) {
+      if (proc.exitCode !== 0) {
         ui.updateBottomBar('');
         console.log(url);
         resolve();
       } else {
         resolve();
       }
-      // if (process.platform === 'win32') {
-      //   proc.addListener('close', () => {
-      //     resolve();
-      //   });
-      //   return;
-      // } else if (wait && proc.exitCode !== 0) {
-      //   console.log(url);
-      //   resolve();
-      // } else {
-      //   console.log(`Browser opened to ${new URL(url).origin}`);
-      //   resolve();
-      // }
     });
   });
 };
